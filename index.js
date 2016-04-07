b92125f1b842c0250610bd34057c71593619342b
Nabil Chiheb <chiheb.design@gmail.com>
2016-04-07 23:08:32 +0200

    add socket.io

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/bower_components'));
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
 
-app.get('/', function(request, response) {
-  response.send('sSocket.io')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+server.listen(5000);
