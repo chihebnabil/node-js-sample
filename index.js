3b63e1b0ec12f0384ab792c443af7de19830d974
Nabil Chiheb <chiheb.design@gmail.com>
2016-04-07 23:15:26 +0200

    add socket.io

--- index.js
+++ index.js
@@ -2,15 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
-
-
-
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-
-//app.use(express.static(__dirname + '/bower_components'));
 app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
