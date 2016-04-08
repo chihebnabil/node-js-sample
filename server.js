5d586ddf70c1bbced5f9e779e3db24752c519f13
Nabil Chiheb <chiheb.design@gmail.com>
2016-04-08 17:11:07 +0200

    tets

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ var http = require("http")
 var users;
 var user = {};
 var port = process.env.PORT || 8080;
-httpServer = http.createServer(function (req,res) {
+var express = require('express')
+var app = express()
 
-  res.end('loool')
-
-  console.log("Serveur Demmaré");
+app.set('port', (process.env.PORT || 5000))
 
 
+app.get('/', function(request, response) {
+  response.send('Hello World!')
 })
-httpServer.listen(port)
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+
+// var io = require("socket.io").listen(httpServer)
+//
+//
+// io.sockets.on("connection",function (socket) {
+//
+//   console.log("nouveau user");
+//
+//     socket.on("login",function (me) {
+//
+//
+//
+//       console.log(me);
+//       console.log('---------------------------');
+//
+//
+//
+//     socket.emit('logged')
+//
+//   })
+// })
